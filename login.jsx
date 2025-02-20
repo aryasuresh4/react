0568988b92f516c91fb7ec2522ea39a681b88750
aryasuresh4 <117814991+aryasuresh4@users.noreply.github.com>
2025-02-20 15:16:54 +0530

    Create login.jsx

---
+++ login.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>
+        <body>
+            
+        
+      <main>
+        <div className="log-back">
+        <div className="back">
+        <div className="log">
+            <div className="form">
+                <form action="">
+                    <div className="text">
+                    <input type="text" placeholder='Username'/></div> <br />
+                    <input type="password" name="" id="" placeholder='Password'/>
+                    <br />
+                    <button>LOGIN</button>
+                </form>
+                <div className="bottom">
+                <p>Not registered? <a href="">Create an account</a></p>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+      </main>
+      </body>
+    </div>
+  )
+}
